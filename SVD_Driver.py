846227f292e2c6d11c7b400ce791fecddbf61e57
Alexandra Nava <aenava@ucsc.edu>
2024-03-17 19:21:10 -0700

    m

--- SVD_Driver.py
+++ SVD_Driver.py
@@ -1,20 +1,21 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt 
+
 
 import SVD_functions
 read_file = SVD_functions.read_file
 svd = SVD_functions.svd
 norm = SVD_functions.norm
-reduced_sigma = SVD_functions.reduced_sigma
 save_to_dat = SVD_functions.save_to_dat
-plot_compressed = SVD_functions.plot_compressed
 
 
 if __name__ == "__main__":
   base = "/Users/alexandranava/Desktop/AM213A/Final Project/"
+  save_data = 1
+  show_images = 1
   #-------------------------------------------------
-  '''1
-  Read in Data - 
+  '''1. Read in Data - 
   matrix T is the tranposed data, 
   m is rows in transposed matrix 
   n is columns in transposed matrix
@@ -24,49 +25,76 @@ if __name__ == "__main__":
   matrixT, m, n = read_file(doggy) 
   print("Reading in " + doggy + "...")
   #-------------------------------------------------
-  '''2
-  SVD Decomposition - 
-  ||A - U@Sigma@VT|| # -> 4.021234833774079e-09
+  '''2. SVD Decomposition - 
+  ||A - U@Sigma@VT|| # -> 4.021234833774079e-09 <3
   '''
-  U, Sigma, VT = svd(matrixT)
+  U, Sigma, s, VT = svd(matrixT)
   U = U.copy()
   VT = VT.copy()
   Sigma = Sigma.copy()
+  s = s.copy()
   #-------------------------------------------------
-  '''3 and 4
-  Reconstructing Compressed Images:
+  '''3. Reconstructing Compressed Images:
   Sigma_k : reduced Sigma
   A_k : A as a reduced SVD
-
   Each reduced SVD is saved to base + "Image_appn_1xxxxx.dat"
   '''
   print("Computing reduced SVDs...")
   ks = [10, 20, 40, 80, 160, 320, 640, 1229]
-  files = []
+  errors = []
   for k in ks:
-    Sigma_k = reduced_sigma(Sigma, k)
-    A_k = U @ Sigma_k @ VT
-    #creating naming
+    A_k = U[:, :k] @ Sigma[0:k, :k] @ VT[:k, :]
+  #-------------------------------------------------
+    '''4. Saving reduced SVDs to .dat files:
+    '''
     tmp_id = ""
     for i in range(5 - len(str(k))):
       tmp_id += "0"
     tmp_id += str(k)
     filename = "Image_appn_1" + tmp_id + ".dat"
-    files.append(filename)
-    #saving compressed A
-    print("Saved reduced SVD with k = " + str(k) + " to " + base + filename + "...")
-    save_to_dat(A_k, base, filename)
+    if save_data == 1:
+      save_to_dat(A_k, base, filename)
+  #-------------------------------------------------
+    '''5. Plotting compressed data
+    '''
+    if show_images == 1:
+      A_k_uint = A_k.copy()
+      A_k_uint8 = A_k_uint.astype(np.uint8)
+      img = plt.imshow(A_k.T)
+      img.set_cmap("grey")
+      plt.axis("off")
+      plt.title("Image Reconstructed with " + str(k) + " Singular Values")
+      plt.savefig(tmp_id + ".jpg")
+  #-------------------------------------------------
+    '''6. Corresponding errors:
+    '''
+    
+    E = norm(matrixT - A_k)/(n*m) #LAST ONE IS NOT 0? 
+    errors.append([k, E])
+  errors_df = pd.DataFrame(errors)
+  errors_df.to_csv("errors.csv") 
+  errors_df.plot(x = 0, y = 1, kind = "line", marker = 'o', color = "pink", label = "Error")
+  plt.xlabel("Number of Singular Values")
+  plt.ylabel("Error")
+  plt.title("Error(Singular Values)")
+  plt.savefig("errors.jpg")
+  plt.close()
 
-  '''
-  5 Plotting compressed data
-  '''
-  for file in files:
-    compressed_data, m_k, n_k = read_file(file)
-    title = file.replace("Image_appn_1", '')
-    title = title.replace(".dat", "")
-    while title[0] == "0":
-      title = title[1:]
-    k = title
-    plot_compressed(compressed_data, title)
+  '''produce original image'''
+  img = plt.imshow(matrixT.T)
+  img.set_cmap("grey")
+  plt.axis("off")
+  plt.title("Original Doggy")
+  plt.savefig("puppy.jpg")
 
+  print("First 10 singular values:")
+  print(s[0:10])
+
+  for k in ks:
+    print("singular value for k = " + str(k))
+    print(s[k-1])
+
+
+#ref 
+#https://www.youtube.com/watch?v=H7qMMudo3e8&t=305s
 
--- SVD_functions.py
+++ SVD_functions.py
@@ -16,11 +16,11 @@ also returns rows m and columns n
 '''
 def read_file(myFileName):  
   with open(myFileName, 'r') as myFile:
-        #data = myFile.read()
-        matrix = np.loadtxt(myFileName)
-        matrix = matrix.T
-        m, n = np.shape(matrix)
-        return matrix, m, n
+    #data = myFile.read()
+    matrix = np.loadtxt(myFileName)
+    matrix = matrix.T
+    m, n = np.shape(matrix)
+    return matrix, m, n
   
 '''
 returns 
@@ -31,23 +31,11 @@ info: 0 if it works, 1 else
 ''' 
 def svd(matrix):
   U, s, VT, info = lapack.dgesvd(matrix)
-  if info == 0:
-    print("SVD successful...")
+  Sigma = np.diag(s)
   Sigma = np.zeros((len(U), len(VT)))
   Sigma_diags = np.diag(s)
   Sigma[:len(s), :len(s)] = Sigma_diags
-  return U, Sigma, VT
-
-'''
-takes in entire matrix Sigma and only keeps first k diagonals,
-setting the rest to 0
-'''
-def reduced_sigma(Sigma, k):
-  Sigma_k = np.zeros_like(Sigma)
-  s = np.diag(Sigma)
-  s_k = s[:k]
-  Sigma_k[:len(s_k), :len(s_k)] = s_k
-  return Sigma_k
+  return U, Sigma, s, VT
 
 '''
 saves matrix as .dat file to 
@@ -58,9 +46,3 @@ def save_to_dat(matrix, base, filename):
 '''
 takes in matrices and title of plot
 '''
-def plot_compressed(matrix, title):
-  compressed_data_uint8 = matrix.astype(np.uint8)
-  plt.imshow(compressed_data_uint8)
-  plt.title(compressed_data_uint8)
-  #plt.colorbar()
-  plt.show()
